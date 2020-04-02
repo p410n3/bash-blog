b1fe6611350170817e86e262e51946f2e19903e2
p410n3 <p410n3@gmail.com>
2020-04-03 00:26:54 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bash-blog
-Make a static blog with a simple bash script 
+## Make a static, completely self contained blog with a simple bash script 
+
+Put posts as markdown in posts, pages as markdown in pages, images as images in images and then run bb.sh
+
+## Template
+
+Just write a simple html and place +++ where the generated stuff should go
--- bb.sh
+++ bb.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# Check if output exists. If not, create it
+[ -d 'output' ] || mkdir 'output'
+
+# Extract top and bottom of template
+# Files are now template00 and template01
+csplit -f template template.html '/+++/' > /dev/null
+
+# Convert all posts from .md to html in the order new to old
+_posts=$(ls posts | sort -r | sed 's#^#posts/#g' | parallel 'pandoc' | parallel 'echo \<div\>{}\<\/div\>')
+
+# Merge that stuff
+echo $(cat template00) $_posts $(cat template01) >> output/index.html
+
+# Show Finished to the user
+echo 'Finished'
+exit 0
